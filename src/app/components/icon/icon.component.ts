adb104cc6c4b5166012fc34b9173bef9cf0771e6
Kendrick <kendrick.oppong@amalitech.com>
2024-12-28 10:57:32 +0300

    chore: setup layout for routing

--- src/app/components/icon/icon.component.ts
+++ src/app/components/icon/icon.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { IconData } from '@interfaces/index';
 
 @Component({
   selector: 'app-icon',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './icon.component.html',
-  styleUrl: './icon.component.css'
+  styleUrl: './icon.component.css',
 })
+  
 export class IconComponent {
-
+  @Input({ required: true }) iconData!: IconData;
 }
