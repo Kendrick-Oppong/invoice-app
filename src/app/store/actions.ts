5f0af3c16bb75a604f5bd002cde5fdd7f852cde1
Kendrick <kendrick.oppong@amalitech.com>
2025-01-01 18:50:58 +0300

    feat: add reducers and actions

---
+++ src/app/store/actions.ts
@@ -0,0 +1,23 @@
+import { Invoice } from '@interfaces/index';
+import { createActionGroup, props, emptyProps } from '@ngrx/store';
+
+export const invoiceActions = createActionGroup({
+  source: 'Invoices',
+  events: {
+    'Add Invoice': props<{ invoice: Invoice }>(),
+    'Add Invoice Success': props<{ invoice: Invoice }>(),
+    'Add Invoice Failure': props<{ error: string }>(),
+
+    'Update Invoice': props<{ id: string; invoice: Invoice }>(),
+    'Update Invoice Success': props<{ invoice: Invoice }>(),
+    'Update Invoice Failure': props<{ error: string }>(),
+
+    'Delete Invoice': props<{ id: string }>(),
+    'Delete Invoice Success': props<{ id: string }>(),
+    'Delete Invoice Failure': props<{ error: string }>(),
+
+    'Load Invoices': emptyProps(),
+    'Load Invoices Success': props<{ invoices: Invoice[] }>(),
+    'Load Invoices Failure': props<{ error: string }>(),
+  },
+});
---
+++ src/app/store/reducers.ts
@@ -0,0 +1,101 @@
+import { createFeature, createReducer, on } from '@ngrx/store';
+import { InvoiceState } from '@interfaces/index';
+import { invoiceActions } from './actions';
+import { produce } from 'immer';
+
+export const initialState: InvoiceState = {
+  invoices: [],
+  loading: false,
+  error: null,
+};
+
+export const InvoiceFeature = createFeature({
+  name: 'Invoices',
+  reducer: createReducer(
+    initialState,
+
+    // Load Invoices
+    on(invoiceActions.loadInvoices, (state) =>
+      produce(state, (draft) => {
+        draft.loading = true;
+        draft.error = null;
+      })
+    ),
+    on(invoiceActions.loadInvoicesSuccess, (state, { invoices }) =>
+      produce(state, (draft) => {
+        draft.invoices = invoices;
+        draft.loading = false;
+      })
+    ),
+    on(invoiceActions.loadInvoicesFailure, (state, { error }) =>
+      produce(state, (draft) => {
+        draft.error = error;
+        draft.loading = false;
+      })
+    ),
+
+    on(invoiceActions.addInvoice, (state) =>
+      produce(state, (draft) => {
+        draft.loading = true;
+        draft.error = null;
+      })
+    ),
+    on(invoiceActions.addInvoiceSuccess, (state, { invoice }) =>
+      produce(state, (draft) => {
+        draft.invoices.push(invoice);
+        draft.loading = false;
+      })
+    ),
+    on(invoiceActions.addInvoiceFailure, (state, { error }) =>
+      produce(state, (draft) => {
+        draft.error = error;
+        draft.loading = false;
+      })
+    ),
+
+    on(invoiceActions.updateInvoice, (state) =>
+      produce(state, (draft) => {
+        draft.loading = true;
+        draft.error = null;
+      })
+    ),
+    on(invoiceActions.updateInvoiceSuccess, (state, { invoice }) =>
+      produce(state, (draft) => {
+        const index = draft.invoices.findIndex(
+          (item) => item.id === invoice.id
+        );
+        if (index !== -1) {
+          draft.invoices[index] = invoice;
+        }
+        draft.loading = false;
+      })
+    ),
+    on(invoiceActions.updateInvoiceFailure, (state, { error }) =>
+      produce(state, (draft) => {
+        draft.error = error;
+        draft.loading = false;
+      })
+    ),
+
+    on(invoiceActions.deleteInvoice, (state) =>
+      produce(state, (draft) => {
+        draft.loading = true;
+        draft.error = null;
+      })
+    ),
+    on(invoiceActions.deleteInvoiceSuccess, (state, { id }) =>
+      produce(state, (draft) => {
+        draft.invoices = draft.invoices.filter((item) => item.id !== id);
+        draft.loading = false;
+      })
+    ),
+    on(invoiceActions.deleteInvoiceFailure, (state, { error }) =>
+      produce(state, (draft) => {
+        draft.error = error;
+        draft.loading = false;
+      })
+    )
+  ),
+});
+
+export const { selectInvoices, selectLoading, selectError } = InvoiceFeature;
