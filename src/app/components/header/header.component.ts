2939d1a5138544793929da53b36e0114239976b1
Kendrick <kendrick.oppong@amalitech.com>
2025-01-02 09:19:04 +0000

    fix: add dynamic invoice count

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HeadlineComponent } from '@components/headline/headline.component';
 import { TextComponent } from '@components/text/text.component';
 import { FilterComponent } from '@components/filter/filter.component';
 import { ButtonComponent } from '@components/button/button.component';
+import { Store } from '@ngrx/store';
+import { selectInvoices } from '@app/store/reducers';
 
 @Component({
   selector: 'app-header',
@@ -11,4 +13,7 @@ import { ButtonComponent } from '@components/button/button.component';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  private store: Store = inject(Store);
+  readonly invoices = this.store.selectSignal(selectInvoices);
+}
