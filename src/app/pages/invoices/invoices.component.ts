607bf131a51497505a20a1507fe3e883a6af9d68
Kendrick <kendrick.oppong@amalitech.com>
2025-01-02 09:21:54 +0000

    feat: add invoices page

--- src/app/pages/invoices/invoices.component.ts
+++ src/app/pages/invoices/invoices.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { invoiceActions } from '@app/store/actions';
+import {
+  selectInvoices,
+  selectLoading,
+  selectError,
+} from '@app/store/reducers';
 
 import { HeaderComponent } from '@components/header/header.component';
+import { InvoiceCardComponent } from '@components/invoice-card/invoice-card.component';
+import { IconComponent } from "@components/icon/icon.component";
+import { ICONS } from '@constants/index';
+import { HeadlineComponent } from "../../components/headline/headline.component";
+import { TextComponent } from "../../components/text/text.component";
 
 @Component({
   selector: 'app-invoices',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, InvoiceCardComponent, IconComponent, HeadlineComponent, TextComponent],
   templateUrl: './invoices.component.html',
-  styleUrl: './invoices.component.css',
+  styleUrls: ['./invoices.component.css'],
 })
-export class InvoicesComponent {}
+export class InvoicesComponent implements OnInit {
+  private store: Store = inject(Store);
+  icons = ICONS;
+  invoices = this.store.selectSignal(selectInvoices);
+  loading = this.store.selectSignal(selectLoading);
+  error = this.store.selectSignal(selectError);
+
+  ngOnInit(): void {
+    this.store.dispatch(invoiceActions.loadInvoices());
+    console.log('invoices', this.invoices());
+    console.log('loading', this.loading());
+    console.log('error', this.error());
+  }
+}
