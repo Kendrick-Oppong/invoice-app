17307db0ba217eae7bdef086b989d0ba4e4eebd2
Kendrick <kendrick.oppong@amalitech.com>
2025-01-06 11:35:17 +0000

    feat: dispatch action to toggle invoice form

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,8 +1,10 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { IconComponent } from '@components/icon/icon.component';
 import { ICONS } from '@constants/index';
 import { HeadlineComponent } from '@components/headline/headline.component';
 import { ButtonType } from '@interfaces/index';
+import { Store } from '@ngrx/store';
+import { invoiceActions } from '@app/store/actions';
 
 @Component({
   selector: 'app-button',
@@ -14,4 +16,9 @@ import { ButtonType } from '@interfaces/index';
 export class ButtonComponent {
   readonly icons = ICONS;
   variant = input.required<ButtonType>();
+  private store: Store = inject(Store);
+
+  addInvoice() {
+    this.store.dispatch(invoiceActions.showAddInvoiceForm());
+  }
 }
