9b6a34567c0e9666ac5e946170282e79d31faa7e
Kendrick <kendrick.oppong@amalitech.com>
2025-01-09 09:28:18 +0000

    feat: implement theme toggle

--- src/app/components/theme-toggle/theme-toggle.component.ts
+++ src/app/components/theme-toggle/theme-toggle.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ICONS } from '@constants/index';
+import { IconComponent } from '@components/icon/icon.component';
+import { ThemeService } from '@app/services/theme/theme.service';
+import { selectIsDarkTheme } from '@app/store/reducers';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-theme-toggle',
   standalone: true,
-  imports: [],
+  imports: [IconComponent],
   templateUrl: './theme-toggle.component.html',
-  styleUrl: './theme-toggle.component.css'
+  styleUrl: './theme-toggle.component.css',
 })
 export class ThemeToggleComponent {
+  private readonly themeService: ThemeService = inject(ThemeService);
+  private readonly store: Store = inject(Store);
+  readonly iconData = ICONS;
+  isDarkMode = this.store.selectSignal(selectIsDarkTheme);
 
+  // Toggle theme on button click
+  toggleTheme(): void {
+    this.themeService.toggleTheme();
+  }
 }
