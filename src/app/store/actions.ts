c1455b262509a8e1d99b4a0acedfb01b28e40844
Kendrick <kendrick.oppong@amalitech.com>
2025-01-17 17:26:05 +0000

    chore: setup effects for actions

--- src/app/store/actions.ts
+++ src/app/store/actions.ts
@@ -1,19 +1,27 @@
-import { Invoice } from '@interfaces/index';
+import { BadgeStatus, Invoice } from '@interfaces/index';
 import { createActionGroup, props, emptyProps } from '@ngrx/store';
 
 export const invoiceActions = createActionGroup({
   source: 'Invoices',
   events: {
+    'Sign In': props<{ username: string; password: string }>(),
+    'Sign In Success': props<{ token: string }>(),
+    'Sign In Failure': props<{ error: string }>(),
+
     'Add Invoice': props<{ invoice: Invoice }>(),
     'Add Invoice Success': props<{ invoice: Invoice }>(),
     'Add Invoice Failure': props<{ error: string }>(),
+
+    'Load Invoice Details': props<{ invoiceId: string }>(),
+    'Load Invoice Details Success': props<{ invoice: Invoice }>(),
+    'Load Invoice Details Failure': props<{ error: string }>(),
 
     'Update Invoice': props<{ id: string; invoice: Invoice }>(),
     'Update Invoice Success': props<{ invoice: Invoice }>(),
     'Update Invoice Failure': props<{ error: string }>(),
 
     'Delete Invoice': props<{ id: string }>(),
-    'Delete Invoice Success': props<{ id: string }>(),
+    'Delete Invoice Success': props<{ message: string }>(),
     'Delete Invoice Failure': props<{ error: string }>(),
 
     'Load Invoices': emptyProps(),
@@ -22,7 +30,10 @@ export const invoiceActions = createActionGroup({
 
     'Filter Invoices': props<{ statuses: string[] }>(),
     'Show Add Invoice Form': emptyProps(),
-    'Mark Invoice As Paid': props<{ id: string }>(),
+
+    'Mark Invoice As Paid': props< { id: string; status: BadgeStatus; invoice: Invoice }>(),
+    'Mark Invoice As Paid Success': props<{ message: string }>(),
+    'Mark Invoice As Paid Failure': props<{ error: string }>(),
   },
 });
 
