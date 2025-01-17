c862d4286333ea4701667ddaa4051c9f5bce6816
Kendrick <kendrick.oppong@amalitech.com>
2025-01-17 17:29:03 +0000

    feat: add sign in route

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { invoiceActions } from '@app/store/actions';
 import { TextFieldComponent } from '@components/text-field/text-field.component';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-sign-in',
@@ -12,15 +14,32 @@ import { TextFieldComponent } from '@components/text-field/text-field.component'
 })
 export class SignInComponent {
   private readonly fb: FormBuilder = inject(FormBuilder);
+  private readonly store = inject(Store);
+  emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   signInForm = this.fb.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', Validators.required],
+    email: [
+      'kendrick.oppong@amalitech.com',
+      [
+        Validators.required,
+        Validators.email,
+        Validators.pattern(this.emailPattern),
+      ],
+    ],
+    password: ['T[<?\\+DyKk~+', [Validators.required, Validators.min(2)]],
   });
   onSubmit() {
     if (this.signInForm.invalid) {
       this.signInForm.markAllAsTouched();
       return;
     }
-    alert('Form submitted successfully');
+
+    const { email, password } = this.signInForm.value;
+    // alert('Form submitted successfully');
+    this.store.dispatch(
+      invoiceActions.signIn({
+        username: email!,
+        password: password!,
+      })
+    );
   }
 }
