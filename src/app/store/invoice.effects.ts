5ea0cae345678c25a30b8e558cb8feb4fc3f253d
Kendrick <kendrick.oppong@amalitech.com>
2025-01-17 17:26:45 +0000

    feat: add crud services

--- src/app/store/invoice.effects.ts
+++ src/app/store/invoice.effects.ts
@@ -1,166 +1,115 @@
 import { inject, Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, map, switchMap, of, delay } from 'rxjs';
+import { catchError, map, switchMap, of } from 'rxjs';
 import { invoiceActions } from '@app/store/actions';
 import { InvoicesService } from '@app/services/invoices/invoices.service';
 import { LocalStorageService } from '@app/services/localstorage/local-storage.service';
-import { BadgeStatus, Invoice } from '@interfaces/index';
-import { Store } from '@ngrx/store';
 import { NotificationService } from '@app/services/notification/notification.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
 
 @Injectable()
 export class InvoiceEffects {
   private readonly actions$: Actions = inject(Actions);
-  private readonly invoicesService: InvoicesService = inject(InvoicesService);
+  private readonly router: Router = inject(Router);
   private readonly store: Store = inject(Store);
+  private readonly invoicesService: InvoicesService = inject(InvoicesService);
   private readonly localStorageService: LocalStorageService =
     inject(LocalStorageService);
   private readonly notificationService: NotificationService =
     inject(NotificationService);
 
-  loadInvoices$ = createEffect(() =>
+  signIn$ = createEffect(() =>
     this.actions$.pipe(
-      ofType(invoiceActions.loadInvoices),
-      switchMap(() => {
-        // Check if invoices are already present in the store or localStorage
-        const invoicesFromStorage =
-          this.localStorageService.getItem<Invoice[]>('invoices');
-        const invoicesInitialized = this.localStorageService.getItem<boolean>(
-          'invoicesInitialized'
-        );
-
-        // If there are invoices in storage and the flag is set, don't fetch
-        if (invoicesFromStorage && invoicesInitialized) {
-          return of(
-            invoiceActions.loadInvoicesSuccess({
-              invoices: invoicesFromStorage,
-            })
-          );
-        }
-
-        // If not initialized, fetch from the JSON file
-        return this.invoicesService.getInvoices().pipe(
-          delay(1000),
-          map((invoices) => {
-            if (invoices.length === 0) {
-              throw new Error('No invoices available');
-            }
-            // Store the fetched invoices and set the initialization flag
-            this.localStorageService.setItem('invoices', invoices);
-            this.localStorageService.setItem('invoicesInitialized', true);
-
-            return invoiceActions.loadInvoicesSuccess({ invoices });
+      ofType(invoiceActions.signIn),
+      switchMap(({ username, password }) =>
+        this.invoicesService.signIn({ username, password }).pipe(
+          map(({ token }) => {
+            this.localStorageService.setItem('authToken', token);
+            this.notificationService.showSuccess('Sign In Successful');
+            this.router.navigate(['/invoices']);
+            return invoiceActions.signInSuccess({ token });
           }),
-          catchError((error) =>
-            of(
-              invoiceActions.loadInvoicesFailure({
-                error: error.message || 'Something went wrong',
-              })
-            )
-          )
-        );
-      })
+          catchError((error: HttpErrorResponse) => {
+            const errorMessage = error.error || 'Invalid Credentials';
+            this.notificationService.showError(errorMessage);
+            return of(invoiceActions.signInFailure({ error: errorMessage }));
+          })
+        )
+      )
     )
   );
 
-  // Effect to add an invoice
-  // addInvoice$ = createEffect(() =>
-  //   this.actions$.pipe(
-  //     ofType(invoiceActions.addInvoice),
-  //     switchMap(({ invoice }) =>
-  //       this.invoicesService.addInvoice(invoice).pipe(
-  //         delay(1000),
-  //         map((newInvoice) => {
-  //           // Add the new invoice to localStorage after successful addition
-  //           const updatedInvoices = [
-  //             ...(this.localStorageService.getItem<Invoice[]>('invoices') ||
-  //               []),
-  //             newInvoice,
-  //           ];
-  //           this.localStorageService.setItem('invoices', updatedInvoices);
+  loadInvoices$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(invoiceActions.loadInvoices),
+      switchMap(() =>
+        this.invoicesService.getInvoices().pipe(
+          map((invoices) => {
+            this.notificationService.showSuccess(
+              'Invoices loaded successfully'
+            );
+            return invoiceActions.loadInvoicesSuccess({ invoices });
+          }),
+          catchError((error: HttpErrorResponse) => {
+            const errorMessage = error.error || 'Failed to load invoices';
+            this.notificationService.showError(errorMessage);
+            return of(
+              invoiceActions.loadInvoicesFailure({ error: errorMessage })
+            );
+          })
+        )
+      )
+    )
+  );
 
-  //           // Dispatch success action
-  //           this.store.dispatch(
-  //             invoiceActions.addInvoiceSuccess({ invoice: newInvoice })
-  //           );
-
-  //           // Dispatch the loadInvoices action to refetch the invoices
-  //           this.store.dispatch(invoiceActions.loadInvoices());
-
-  //           return invoiceActions.addInvoiceSuccess({ invoice: newInvoice });
-  //         }),
-  //         catchError((error) =>
-  //           of(invoiceActions.addInvoiceFailure({ error: error.message }))
-  //         )
-  //       )
-  //     )
-  //   )
-  // );
   addInvoice$ = createEffect(() =>
     this.actions$.pipe(
       ofType(invoiceActions.addInvoice),
       switchMap(({ invoice }) =>
-        this.invoicesService.addInvoice(invoice).pipe(
-          delay(1000),
-          map((newInvoice) => {
-            const updatedInvoices = [
-              newInvoice,
-              ...(this.localStorageService.getItem<Invoice[]>('invoices') ||
-                []),
-            ];
-            this.localStorageService.setItem('invoices', updatedInvoices);
-
-            // Dispatch success action and notification
-            this.store.dispatch(
-              invoiceActions.addInvoiceSuccess({ invoice: newInvoice })
-            );
-            this.notificationService.showSuccess('Invoice added successfully');
-
-            //hide form
+        this.invoicesService.addInvoice({ invoice }).pipe(
+          map((addedInvoice) => {
+            const successMessage = 'Invoice added successfully';
+            this.notificationService.showSuccess(successMessage);
             this.store.dispatch(invoiceActions.showAddInvoiceForm());
-
-            // Dispatch loadInvoices action to refresh the list
-            return invoiceActions.loadInvoices();
+            setTimeout(() => {
+              this.router.navigate(['/invoices']);
+            }, 2000);
+            this.store.dispatch(invoiceActions.loadInvoices());
+            return invoiceActions.addInvoiceSuccess({ invoice: addedInvoice });
           }),
-          catchError((error) => {
-            this.notificationService.showError('Failed to add invoice');
+          catchError((error: HttpErrorResponse) => {
+            const errorMessage = error.error || 'Failed to add invoice';
+            this.notificationService.showError(errorMessage);
             return of(
-              invoiceActions.addInvoiceFailure({ error: error.message })
+              invoiceActions.addInvoiceFailure({ error: errorMessage })
             );
           })
         )
       )
     )
   );
-  // Effect to update an invoice
+
   updateInvoice$ = createEffect(() =>
     this.actions$.pipe(
       ofType(invoiceActions.updateInvoice),
       switchMap(({ id, invoice }) =>
-        of(void 0).pipe(
-          delay(1000),
-          map(() => {
-            const invoices =
-              this.localStorageService.getItem<Invoice[]>('invoices') || [];
-
-            const updatedInvoices = invoices.map((item) =>
-              item.id === id ? { ...item, ...invoice } : item
-            );
-
-            this.localStorageService.setItem('invoices', updatedInvoices);
-
-            this.notificationService.showSuccess('Invoice updated successfully');
-
-            // Dispatch loadInvoices action to refresh the list
-            this.store.dispatch(invoiceActions.loadInvoices());
-
+        this.invoicesService.updateInvoice({ id, invoice }).pipe(
+          map((updatedInvoice) => {
+            const successMessage = 'Invoice updated successfully';
+            this.notificationService.showSuccess(successMessage);
+            this.router.navigate(['/invoices']);
             this.store.dispatch(invoiceActions.showAddInvoiceForm());
-            return invoiceActions.updateInvoiceSuccess({ invoice });
+            return invoiceActions.updateInvoiceSuccess({
+              invoice: updatedInvoice,
+            });
           }),
-          catchError((error) => {
-            this.notificationService.showError('Failed to update invoice');
+          catchError((error: HttpErrorResponse) => {
+            const errorMessage = error.error || 'Failed to update invoice';
+            this.notificationService.showError(errorMessage);
             return of(
-              invoiceActions.updateInvoiceFailure({ error: error.message })
+              invoiceActions.updateInvoiceFailure({ error: errorMessage })
             );
           })
         )
@@ -168,66 +117,77 @@ export class InvoiceEffects {
     )
   );
 
-  deleteInvoice$ = createEffect(() =>
+  invoiceDetails$ = createEffect(() =>
     this.actions$.pipe(
+      ofType(invoiceActions.loadInvoiceDetails),
+      switchMap(({ invoiceId }) =>
+        this.invoicesService.getInvoiceById({ invoiceId }).pipe(
+          map((loadedInvoice) => {
+            this.notificationService.showSuccess('Invoice loaded successfully');
+            return invoiceActions.loadInvoiceDetailsSuccess({
+              invoice: loadedInvoice,
+            });
+          }),
+          catchError((error: HttpErrorResponse) => {
+            const errorMessage =
+              error.error || 'Failed to load invoice details';
+            this.notificationService.showError(errorMessage);
+            return of(
+              invoiceActions.loadInvoiceDetailsFailure({ error: errorMessage })
+            );
+          })
+        )
+      )
+    )
+  );
+  deleteInvoice$ = createEffect(() => {
+    return this.actions$.pipe(
       ofType(invoiceActions.deleteInvoice),
       switchMap(({ id }) =>
-        of(void 0).pipe(
-          delay(1000),
+        this.invoicesService.deleteInvoice({ id }).pipe(
           map(() => {
-            const currentInvoices =
-              this.localStorageService.getItem<Invoice[]>('invoices') || [];
-            const updatedInvoices = currentInvoices.filter(
-              (invoice) => invoice.id !== id
-            );
-            this.localStorageService.setItem('invoices', updatedInvoices);
-
-            this.notificationService.showSuccess(
-              'Invoice deleted successfully'
-            );
-
-            return invoiceActions.deleteInvoiceSuccess({ id });
+            const successMessage = 'Invoice deleted successfully';
+            this.notificationService.showSuccess(successMessage);
+            this.router.navigate(['/invoices']);
+            return invoiceActions.deleteInvoiceSuccess({
+              message: successMessage,
+            });
           }),
-          catchError((error) => {
-            this.notificationService.showError('Failed to delete invoice');
+          catchError((error: HttpErrorResponse) => {
+            const errorMessage = error.error || 'Failed to delete invoice';
+            this.notificationService.showError(errorMessage);
             return of(
-              invoiceActions.deleteInvoiceFailure({
-                error: error.message || 'Failed to delete invoice',
-              })
+              invoiceActions.deleteInvoiceFailure({ error: errorMessage })
             );
           })
         )
       )
-    )
-  );
+    );
+  });
 
-  markInvoiceAsPaid$ = createEffect(() =>
-    this.actions$.pipe(
+  MarkAsPaid$ = createEffect(() => {
+    return this.actions$.pipe(
       ofType(invoiceActions.markInvoiceAsPaid),
-      map(({ id }) => {
-        const invoices =
-          this.localStorageService.getItem<Invoice[]>('invoices') || [];
-        const updatedInvoices = invoices.map((invoice) =>
-          invoice.id === id
-            ? { ...invoice, status: 'paid' as BadgeStatus }
-            : invoice
-        );
-        this.localStorageService.setItem('invoices', updatedInvoices);
-        this.notificationService.showSuccess('Invoice marked as paid');
-        const updatedInvoice = updatedInvoices.find(
-          (invoice) => invoice.id === id
-        );
-        if (!updatedInvoice) {
-          throw new Error('Updated invoice not found');
-        }
-        return invoiceActions.updateInvoiceSuccess({ invoice: updatedInvoice });
-      }),
-      catchError((error) => {
-        this.notificationService.showError('Failed to mark invoice as paid');
-        return of(
-          invoiceActions.updateInvoiceFailure({ error: error.message })
-        );
-      })
-    )
-  );
+      switchMap(({ id, invoice, status }) =>
+        this.invoicesService.markAsPaid({ id, invoice, status }).pipe(
+          map((invoice) => {
+            const successMessage = 'Invoice marked as paid successfully';
+            this.notificationService.showSuccess(successMessage);
+            invoiceActions.loadInvoiceDetailsSuccess({ invoice });
+            return invoiceActions.markInvoiceAsPaidSuccess({
+              message: successMessage,
+            });
+          }),
+          catchError((error: HttpErrorResponse) => {
+            const errorMessage =
+              error.error || 'Failed to mark invoice as paid';
+            this.notificationService.showError(errorMessage);
+            return of(
+              invoiceActions.markInvoiceAsPaidFailure({ error: errorMessage })
+            );
+          })
+        )
+      )
+    );
+  });
 }
